6313abcc755c972fa375e81bb39f22153317f023
Yuvraj Suri <yuvrajsuri32@gmail.com>
2026-05-11 23:43:43 +0530

    added new function in UserServiceImpl

--- src/main/java/com/yuvraj/SimplyForge/Service/Impl/UserServiceImpl.java
+++ src/main/java/com/yuvraj/SimplyForge/Service/Impl/UserServiceImpl.java
@@ -1,5 +1,9 @@
 package com.yuvraj.SimplyForge.Service.Impl;
 
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.yuvraj.SimplyForge.Service.UserService;
@@ -13,5 +17,17 @@ public class UserServiceImpl implements UserService{
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getProfile'");
     }
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return new JwtUserPrincipal(
+                user.getId(),
+                user.getName(),
+                user.getUsername(),
+                user.getPassword(),
+                new ArrayList<>()
+        );    }
 
 }
